59ada7197d4300bdb9a9c5506bc14fdf27da3627
Andrey Kondratyuk <andrey@dsrp.tv>
2019-06-20 15:07:47 -0400

    convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by AndreyK on 6/20/19.
+//  Copyright © 2019 Disruptive Inc. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+  var title: String = ""
+  var done: Bool = false
+}
