67aa59ecf6597906de0233d5af043ae3565386f6
Andrey Kondratyuk <andrey@dsrp.tv>
2019-06-20 12:00:37 -0400

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by AndreyK on 6/20/19.
+//  Copyright © 2019 Disruptive Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
